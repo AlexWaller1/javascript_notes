d5e8876c030614f8e51a406380e725aaa03b2dcc
Alex Waller <alex@artofalexwaller.com>
2021-03-17 17:46:37 -0400

    function notes

--- functions/functions.js
+++ functions/functions.js
@@ -11,3 +11,35 @@ heyBodhi(); */
 // everything inside the {} is run any time the function is called.
 
 // Passing Values to Functions with Arguments
+// parameters act as placeholders for the values that will be
+// inputed to a function
+
+/*
+      function ourFunctionWithArgs(a, b) {
+          console.log(a-b);
+      }
+
+      ourFunctionWithArgs(10, 5); will output 5 */
+
+// Global Scope and Functions
+// scope refers to the visibility of variables
+// global variables refer to variables that can be read anywhere 
+// in the JavaScript application.
+// typeof is JavaScript keyword that will return the type of variable when you call it
+
+/* var myGlobal = 10;
+
+   function fun1() {
+
+   }
+
+   function fun2(){
+       var output = "";
+       if (typeof myGlobal != 'undefined') {
+           output += "myGlobal: " + myGlobal;
+       }
+       if (typeof oopsGlobal != 'undefined') {
+           output += " oopsGlobal: " + oopsGlobal;
+       }
+       console.log(output);
+   }
