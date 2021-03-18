8bf661b1595f8b0c0185a3de64082dc713a87925
Alex Waller <alex@artofalexwaller.com>
2021-03-18 00:18:56 -0400

    adapters

---
+++ adapters/adapters.js
@@ -0,0 +1,6 @@
+// This is where our JavaScript front end will communicate with the backend Api
+// we set up our base Url in the adapter
+// fetch requests are handled in the adapter
+// these fetch requests include create and update requests
+// after we get the hybrid from the backend rails program, will then
+// JSON and stringify the hybrid
--- basics/basics.js
+++ basics/basics.js
@@ -1,3 +1,5 @@
 /* console.log allows us to see things in
 the console, this allows us to check what variables are
 in various parts of our program 
+
+// this.
