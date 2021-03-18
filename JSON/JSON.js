84537db32f4536a0b0bc27a4b62e6c2908b6e22d
Alex Waller <alex@artofalexwaller.com>
2021-03-18 00:33:07 -0400

    JSON

---
+++ JSON/JSON.js
@@ -0,0 +1,22 @@
+// JSON is a format for storing and transporting data.
+// JSON is often used when data is sent from a server to a web page
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent
+// JSON is "self-describing" and easy to understand
+// The JSON format evaluates to JavaScript Objects
+// The JSON format is syntactically identical to the code for creating JavaScript objects
+// Because of this similarity, a JavaScript program can easily convert 
+// JSON data into native JavaScript objects
+// JSON Syntax Rules
+// Data is in name/value pairs
+// Data is separated by commas
+// Curly braces hold objects
+// Square brackets hold arrays
+// Converting a JSON text to a JavaScript object
+// A common use of JSON is to read data from a web server,
+// and display the data in a web page.
+// For simplicity, this can be demonstrated using as input
+// Then, use the JavaScript built-in function JSON.parse() to convert the string
+// into a JavaScript object
+// var obj = JSON.parse(text);
